4de6fe179f68f049018b5b45cafedae4a46c205d
Keepsimple - Jose Junior <keepsimple.jose@unidas.com.br>
2021-12-27 19:35:26 -0300

    cria action que permite executar comandos do heroku

---
+++ README.md
@@ -0,0 +1,6 @@
+# Heroku Deploy
+
+![GitHub issues](https://img.shields.io/github/issues/tiltshift/heroku-promote-app.svg)
+![GitHub](https://img.shields.io/github/license/tiltshift/heroku-promote-app.svg)
+
+This is a very simple GitHub action that allows you to promote an app in a heroku pipeline.
---
+++ index.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core')
+const { execSync } = require('child_process')
+
+// Support Functions
+const createCatFile = ({ email, api_key }) => `cat >~/.netrc <<EOF
+machine api.heroku.com
+    login ${email}
+    password ${api_key}
+machine git.heroku.com
+    login ${email}
+    password ${api_key}
+EOF`
+
+// Input Variables
+let heroku = {}
+heroku.api_key = core.getInput('heroku_api_key')
+heroku.email = core.getInput('heroku_email')
+heroku.app_name = core.getInput('heroku_app_name')
+heroku.command = core.getInput('heroku_command')
+
+// Program logic
+try {
+  execSync(createCatFile(heroku))
+  console.log('Created and wrote to ~./netrc')
+
+  execSync('heroku login')
+  console.log('Successfully logged into heroku')
+
+  execSync(`heroku ${heroku.command}`);
+
+  core.setOutput(
+    'status',
+    'Successfully published on' + heroku.app_name,
+  )
+} catch (err) {
+  core.setFailed(err.toString())
+}
